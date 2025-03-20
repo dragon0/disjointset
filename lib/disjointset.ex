b50da7f36c7d8e3dc6c00404fde577aa41ab2942
Raven <sravendragon0@hotmail.com>
2025-03-19 18:59:16 -0700

    Initial Mix project

---
+++ lib/disjointset.ex
@@ -0,0 +1,18 @@
+defmodule Disjointset do
+  @moduledoc """
+  Documentation for `Disjointset`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Disjointset.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/disjointset_test.exs
@@ -0,0 +1,8 @@
+defmodule DisjointsetTest do
+  use ExUnit.Case
+  doctest Disjointset
+
+  test "greets the world" do
+    assert Disjointset.hello() == :world
+  end
+end
