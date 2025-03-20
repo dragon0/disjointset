5c484d1360fc61496ab39106c0210145e76c0afe
Raven <sravendragon0@hotmail.com>
2025-03-19 20:09:23 -0700

    Initial DisjointSet implementation

--- lib/disjointset.ex
+++ lib/disjointset.ex
@@ -1,18 +1,90 @@
-defmodule Disjointset do
+defmodule DisjointSet do
   @moduledoc """
-  Documentation for `Disjointset`.
+  Documentation for `DisjointSet`.
   """
+
+  defstruct forest: %{}, rank: %{}
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Disjointset.hello()
-      :world
-
+  Creates a new `DisjointSet`.
   """
-  def hello do
-    :world
+  def new() do
+    %DisjointSet{}
+  end
+
+  @doc """
+  Inserts `x` into a `DisjointSet` if it is not already present.
+  """
+  def insert(set = %DisjointSet{forest: forest, rank: rank}, x) do
+    %{set | forest: Map.put_new(forest, x, x), rank: Map.put_new(rank, x, 0)}
+  end
+
+  @doc """
+  Returns the representative for `x`.
+  """
+  def find(set = %DisjointSet{forest: forest}, x) do
+    parent = Map.get(forest, x)
+    if parent != x do
+      find(set, parent)
+    else
+      x
+    end
+  end
+
+  @doc """
+  Returns the representative for `x` and updates all nodes on the path from `x` to its representative to use the same root representative.
+  """
+  def find_and_compress(set = %DisjointSet{forest: forest}, x) do
+    root = find(set, x)
+    forest = compress(forest, x, root)
+    {root, %{set | forest: forest}}
+  end
+
+  #Path compression: Updates all nodes on the path from `x` to `new_root` to all refer to `new_root` as their representative.
+  defp compress(forest, x, new_root) do
+    parent = Map.get(forest, x)
+    if parent != new_root do
+      compress(Map.put(forest, x, new_root), parent, new_root)
+    else
+      forest
+    end
+  end
+
+  @doc """
+  Ensures `x` and `y` are in the same subset by updating them to refer to the same representative. Also compresses paths from `x` and `y` to the new representative.
+  """
+  def union(set, x, y) do
+    x_root = find(set, x)
+    y_root = find(set, y)
+
+    if x_root == y_root do
+      set
+    else
+      x_rank = Map.get(set.rank, x_root)
+      y_rank = Map.get(set.rank, y_root)
+
+      # Swap so the taller tree will be the new root
+      {x_root, y_root} = if x_rank < y_rank do
+        {y_root, x_root}
+      else
+        {x_root, y_root}
+      end
+
+      # Set x to be the new root
+      forest = Map.put(set.forest, y_root, x_root)
+
+      # Compress paths from x and y to point to the new root
+      forest = compress(forest, x, x_root)
+      forest = compress(forest, y, x_root)
+
+      # Update upper bounds of new root
+      rank = if x_rank == y_rank do
+        Map.put(set.rank, x_root, x_rank+1)
+      else
+        set.rank
+      end
+
+      %{set | forest: forest, rank: rank}
+    end
   end
 end
--- test/disjointset_test.exs
+++ test/disjointset_test.exs
@@ -1,8 +1,76 @@
-defmodule DisjointsetTest do
+defmodule DisjointSetTest do
   use ExUnit.Case
-  doctest Disjointset
+  doctest DisjointSet
 
-  test "greets the world" do
-    assert Disjointset.hello() == :world
+  test "creates a set" do
+    assert DisjointSet.new() == %DisjointSet{}
   end
+
+  test "inserts a single value" do
+    set = DisjointSet.new()
+    set = DisjointSet.insert(set, 1)
+    assert Enum.count(set.forest) == 1
+  end
+
+  test "inserts two values" do
+    set = DisjointSet.new()
+    set = DisjointSet.insert(set, 1)
+    set = DisjointSet.insert(set, 2)
+    assert Enum.count(set.forest) == 2
+  end
+
+  test "does not insert duplicates" do
+    set = DisjointSet.new()
+    set = DisjointSet.insert(set, 1)
+    set = DisjointSet.insert(set, 1)
+    assert Enum.count(set.forest) == 1
+  end
+
+  test "has distinct roots for inserted values" do
+    set = DisjointSet.new()
+    set = DisjointSet.insert(set, 1)
+    set = DisjointSet.insert(set, 2)
+    one_root = DisjointSet.find(set, 1)
+    two_root = DisjointSet.find(set, 2)
+    assert one_root != two_root
+  end
+
+  test "unions two roots" do
+    set = DisjointSet.new()
+    set = DisjointSet.insert(set, 1)
+    set = DisjointSet.insert(set, 2)
+    set = DisjointSet.union(set, 1, 2)
+    one_root = DisjointSet.find(set, 1)
+    two_root = DisjointSet.find(set, 2)
+    assert one_root == two_root
+  end
+
+  test "unions several roots" do
+    set = Enum.reduce(1..8, DisjointSet.new(), fn x, set -> DisjointSet.insert(set, x) end)
+
+    set = DisjointSet.union(set, 1, 2)
+    set = DisjointSet.union(set, 1, 5)
+    set = DisjointSet.union(set, 1, 6)
+    set = DisjointSet.union(set, 1, 8)
+    set = DisjointSet.union(set, 3, 4)
+
+    one_root = DisjointSet.find(set, 1)
+    two_root = DisjointSet.find(set, 2)
+    three_root = DisjointSet.find(set, 3)
+    four_root = DisjointSet.find(set, 4)
+    five_root = DisjointSet.find(set, 5)
+    six_root = DisjointSet.find(set, 6)
+    seven_root = DisjointSet.find(set, 7)
+    eight_root = DisjointSet.find(set, 8)
+
+    assert one_root == two_root
+    assert one_root == five_root
+    assert one_root == six_root
+    assert one_root == eight_root
+    assert three_root == four_root
+    assert one_root != three_root
+    assert one_root != seven_root
+    assert three_root != seven_root
+  end
+
 end
